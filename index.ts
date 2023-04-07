f628e24edfb7e47a1c4b340907d33f407324dd9a
Vikash Joshi <joshi.vikash17@gmail.com>
2023-04-07 15:50:29 +0530

    adding more examples of creating Observables
    adding example of creating Observable from the subscribe function

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import { Subject, Observable } from 'rxjs';
 const myObservable = new Observable<number>((producer) => {
   producer.next(1);
   producer.next(2);
-  // producer.error({ err: 'this is an error' });
   producer.complete();
 });
 // Error and Complete can happen only once during the execution and either one of them can happen
@@ -13,3 +12,16 @@ const mySubscription = myObservable.subscribe({
   complete: () => console.log('complete'),
 });
 mySubscription.unsubscribe();
+
+const myObservable2 = new Observable<number>(function subscribe(producer) {
+  producer.next(1);
+  producer.next(2);
+  producer.error({ err: 'this is an error' });
+});
+// Error and Complete can happen only once during the execution and either one of them can happen
+const mySubscription2 = myObservable2.subscribe({
+  next: (value) => console.log(value),
+  error: (err) => console.log(err),
+  complete: () => console.log('complete'),
+});
+mySubscription2.unsubscribe();
